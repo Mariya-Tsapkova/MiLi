fbf6f8bd7576b9da9ce846ccc6b20d36f0224370
@Mariya-Tsapkova <mashuta_tsapkova@mail.ru>
2022-07-29 00:04:26 +0300

    Initial commit

---
+++ MaiN.java
@@ -0,0 +1,8 @@
+public class MaiN {
+    public static void main(String[] args) {
+        int ticket = 100;
+        int rub = 20;
+        int miles = ticket / rub;
+        System.out.println("Начислено миль: "+ miles);
+    }
+}
